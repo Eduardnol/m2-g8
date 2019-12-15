9f92f96aab12f37cb078efdb5e2a3164bf33de55
Eduardo <eduardo.nolla@outlook.com>
2019-12-15 11:27:51 +0100

    3-Data and Main

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "model/functions/functions.h"
 #include "model/list/list.h"
 #include "model/destination/destination.h"
 
@@ -8,7 +9,15 @@
 int main() {
 	int option;
 
+	FILE* f = fopen(PATH, "r");
+
+	if (f = NULL) {
+		return -1;
+	}
+
 	List l = create();
+
+	readWholeFile(f, &l);
 
 	destroy(&l);
 
